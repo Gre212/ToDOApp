4fde3317859e51a1eddad76722393d38501e6bf1
Gre212 <57032222+Gre212@users.noreply.github.com>
2022-03-24 11:06:45 +0900

    セミコロンつけた

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,8 +1,8 @@
-import express, { Request, Response }  from 'express'
+import express, { Request, Response }  from 'express';
 
 export const app = express();
 
-import { router as tasks } from './apis/tasks'
+import { router as tasks } from './apis/tasks';
 
 app.get('/', (_: Request, res: Response) => {
   return res.json({"Updated": "Hello World!"});
