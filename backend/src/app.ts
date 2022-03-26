90a9f454c20b21779d2c25c313a774ccfa89abf1
Gre212 <57032222+Gre212@users.noreply.github.com>
2022-03-26 15:24:34 +0900

    CROSの対応

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,10 +1,15 @@
 // Modules
 import express, { Request, Response }  from 'express';
+import cors from 'cors';
 
 // API endpoints
 import { router as tasks } from './apis/tasks';
 
 export const app = express();
+app.use(cors({
+  origin: "'*'",
+  allowedHeaders: "'Content-Type,x-requested-with'"
+}));
 app.get('/', (_: Request, res: Response) => {
   return res.json({"Updated": "Hello World!"});
 });
