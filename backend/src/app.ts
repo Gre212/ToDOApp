80b43c55beef7963fddf8050e9e239384e54a8af
Gre212 <57032222+Gre212@users.noreply.github.com>
2022-03-28 18:17:53 +0900

    Sentryへレポートするように設定（公式サンプルコードまま）

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,11 +1,35 @@
 // Modules
 import express, { Request, Response }  from 'express';
 import cors from 'cors';
+import * as Sentry from "@sentry/node";
+import * as Tracing from "@sentry/tracing";
 
 // API endpoints
 import { router as tasks } from './apis/tasks';
 
 export const app = express();
+
+Sentry.init({
+  dsn: process.env.SENTRY_DNS,
+  integrations: [
+    // enable HTTP calls tracing
+    new Sentry.Integrations.Http({ tracing: true }),
+    // enable Express.js middleware tracing
+    new Tracing.Integrations.Express({ app }),
+  ],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
+// RequestHandler creates a separate execution context using domains, so that every
+// transaction/span/breadcrumb is attached to its own Hub instance
+app.use(Sentry.Handlers.requestHandler());
+// TracingHandler creates a trace for every incoming request
+app.use(Sentry.Handlers.tracingHandler());
+
+
 app.use(cors({
   origin: "'*'",
   allowedHeaders: "'Content-Type,x-requested-with'"
@@ -25,3 +49,6 @@ app.get('/', (_: Request, res: Response) => {
   });
 });
 app.use('/tasks', tasks);
+
+// The error handler must be before any other error middleware and after all controllers
+app.use(Sentry.Handlers.errorHandler());
