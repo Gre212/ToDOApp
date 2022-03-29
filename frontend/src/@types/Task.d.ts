52a79b054b75ff3030daba999840741ae73e2085
Gre212 <57032222+Gre212@users.noreply.github.com>
2022-03-29 22:47:35 +0900

    バックエンドの仕様変更に伴う型の修正

--- frontend/src/@types/Task.d.ts
+++ frontend/src/@types/Task.d.ts
@@ -1,8 +1,9 @@
 export declare interface Task {
-  id: string | null,
-  title: string,
-  content: string,
-  limit: string, // TODO: "yyyy-mm-dd" の形式に制限する
-  state: string // TODO: 受け入れるステータスは定義済みのもののみなので制限する
+  Id: string | null,
+  Title: string,
+  Content: string,
+  Limit: string, // TODO: "yyyy-mm-dd" の形式に制限する
+  State: string // TODO: 受け入れるステータスは定義済みのもののみなので制限する
+  CreatedAt: string
 }
 
