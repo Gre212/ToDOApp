1a3301ae7ef9ed09699cee197b4051b146b45fc0
Gre212 <57032222+Gre212@users.noreply.github.com>
2022-03-29 22:49:23 +0900

    表示＆検索機能の実装

--- frontend/src/components/tasks/Container.tsx
+++ frontend/src/components/tasks/Container.tsx
@@ -1,5 +1,6 @@
-import { Grid } from '@mui/material';
+import { Button, FormControl, Grid, InputLabel, MenuItem, Select, SelectChangeEvent, TextField } from '@mui/material';
 import { useEffect, useState } from 'react';
+import { useThrottle } from "rooks";
 import axios from 'utils/axios';
 import { Task } from '../../@types/Task'; // 相対パスじゃないとエラーになる
 import TaskCard from './Card';
@@ -7,23 +8,56 @@ import TaskCreateButton from './CreateButton';
 
 
 function TaskContainer() {
-  const [tasks, loadTasks] = useState<Task[]>([]);
+  const [tasks, setTasks] = useState<Task[]>([]);
+  const [lastKey, setLastKey] = useState({Id: "", State:"", CreatedAt: ""});
+  const [state, setState] = useState("new");
+  const [query, setQuery] = useState("");
+
+  // throttle させることで、inputフィールドのタイプの度にリクエストが飛ぶのを防ぐ
+  const [throttledGetTasks, isReady] = useThrottle(getTasks, 1000);
 
   useEffect(()=>{
-    getTasks();
-  }, []);
+    throttledGetTasks();
+  }, [state, query]);
   async function getTasks() {
-    const response = await axios.get("/tasks");
+    const response = await axios.get("/tasks", {
+      "params": {
+          "State": state
+        }
+    });
     console.log(response.data.Items);
-    loadTasks(response.data.Items);
+    setTasks(response.data.Items);
+    setLastKey(response.data.LastEvaluatedKey);
+  }
+
+  async function getMoreTasks() {
+    if(lastKey == undefined) return;
+    const response = await axios.get("/tasks", {
+      "params": {
+          "State": state,
+          "lastId": lastKey.Id,
+          "lastState": lastKey.State,
+          "lastCreatedAt": lastKey.CreatedAt
+        }
+    });
+    setTasks(tasks.concat(response.data.Items));
+    setLastKey(response.data.LastEvaluatedKey);
+  }
+
+  function handleStateChange(event: SelectChangeEvent) {
+    setState(event.target.value as string);
+  }
+
+  function handleInputChange(event: React.ChangeEvent<HTMLInputElement>){
+    const target = event.target as HTMLInputElement;
+    setQuery(target.value);
   }
 
   function CardList(){
     const listItems = tasks.map((task) => {
       return (
-        <Grid item xs={6} key={task.id}>
+        <Grid item xs={6} key={task.Id}>
           <TaskCard propTask={task} getTasks={getTasks}/>
-          {/* <TaskModal propTask={task} /> */}
         </Grid>
       )
     });
@@ -33,15 +67,49 @@ function TaskContainer() {
         spacing={2}
         columns={{ xs: 8, md: 10 }}
         justifyContent="center"
+        sx={{ mt:"8px" }}
         >
         {listItems}
-        {/* <TaskModal /> */}
+        { (lastKey != undefined && lastKey.Id != "") &&
+          <Grid item xs={6}>
+            <Button onClick={getMoreTasks}>もっと読み込む</Button>
+          </Grid>
+        }
       </Grid>
     );
   }
 
   return (
     <>
+      <Grid
+        container
+        columns={{ xs: 8, md: 10 }}
+        justifyContent="center">
+        <Grid item xs={3}>
+          <FormControl>
+            <InputLabel id="state">タスクの状態</InputLabel>
+            <Select
+              labelId="state"
+              value={state}
+              name="State"
+              label="タスクの状態"
+              onChange={handleStateChange}>
+              <MenuItem value={"new"}>未着手</MenuItem>
+              <MenuItem value={"progress"}>対応中</MenuItem>
+              <MenuItem value={"done"}>完了済み</MenuItem>
+            </Select>
+          </FormControl>
+        </Grid>
+        <Grid item xs={3}>
+          <TextField
+            type="text"
+            fullWidth id="search" label="タイトルで検索" variant="outlined"
+            name="query"
+            value={query}
+            onChange={handleInputChange}
+            />
+        </Grid>
+      </Grid>
       <CardList />
       <TaskCreateButton getTasks={getTasks}/>
     </>
